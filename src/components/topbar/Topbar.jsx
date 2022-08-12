aa0c151471df9bc254ac4f94371b5365cd997a97
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 21:15:55 -0400

    import person and mail icon and display in left container

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Person, Mail } from "@material-ui/icons";
 import "./Topbar.scss";
 
 export default function Topbar() {
@@ -8,7 +9,14 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             GC
           </a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>647 801 3218</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>gloriacheung812@gmail.com</span>
+          </div>
         </div>
 
         <div className="right"></div>

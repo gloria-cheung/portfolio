4a2279b847c358e31acb97df965228889690b88b
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 20:59:36 -0400

    add id to div

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,6 @@ import "./Portfolio.scss"
 
 export default function Portfolio() {
   return (
-    <div className="portfolio">Portfolio</div>
+    <div className="portfolio" id="portfolio">Portfolio</div>
   )
 }

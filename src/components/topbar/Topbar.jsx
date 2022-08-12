58b80fb5a840c54d012dd24fdba1d5d50aeb34b6
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 21:03:23 -0400

    add anchor tag to link to intro section

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,18 @@
-import "./Topbar.scss"
+import "./Topbar.scss";
 
 export default function Topbar() {
   return (
-    <div className="topbar">Topbar</div>
-  )
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            GC
+          </a>
+          <div className="itemContainer"></div>
+        </div>
+
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }

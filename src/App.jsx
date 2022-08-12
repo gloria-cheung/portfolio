4be67c96899096192c589a6b2dedba06fc115731
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 20:06:43 -0400

    rename to .jsx to use shorthand snippets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="section"></div>
+    </div>
+  );
+}
+
+export default App;

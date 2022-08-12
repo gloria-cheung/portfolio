6711456f0e67b2346d9305261a5e740aa04c0878
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-11 20:16:59 -0400

    import all components and display in section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Education from "./components/education/Education";
+import Contact from "./components/contact/Contact";
+
+import "./App.scss"
 
 function App() {
   return (
     <div className="app">
       <Topbar />
-      <div className="section"></div>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Education/>
+        <Contact/>
+      </div>
     </div>
   );
 }

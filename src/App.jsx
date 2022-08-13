dc7632bce617f31091ad3d048d9939a23c2e3a21
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-13 10:15:18 -0400

    usestate to control when menu is active

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -5,18 +6,24 @@ import Works from "./components/works/Works";
 import Education from "./components/education/Education";
 import Contact from "./components/contact/Contact";
 
-import "./App.scss"
+import "./App.scss";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const openMenuHandler = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar onClick={openMenuHandler} menuOpen={menuOpen} />
       <div className="sections">
-        <Intro/>
-        <Portfolio/>
-        <Works/>
-        <Education/>
-        <Contact/>
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Education />
+        <Contact />
       </div>
     </div>
   );

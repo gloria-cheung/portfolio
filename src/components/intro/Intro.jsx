0948bdfb5e7ac12e1fbb7459f8c7ba9fc6ca9b3a
gloria <macuser@host250.triadfluidpower.com>
2022-08-20 18:19:14 -1000

    split div to right and left container and add markup for pic and description

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,25 @@
-import "./Intro.scss"
+import "./Intro.scss";
 
 export default function Intro() {
   return (
-    <div className="intro" id="intro">Intro</div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/myphoto.png" alt="gloria" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Gloria Cheung</h1>
+          <h3>
+            Full Stack Developer<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }

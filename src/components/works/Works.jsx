0e04fc22207ed51b6aff07b2d4a51d06c6db9884
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-08-28 10:14:37 -0400

    use state to track position of current slide and translate to either left or right based on onclick of arrow

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Works.scss";
 
 export default function Works() {
-  const [currentSlider, setCurrentSlider] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
       id: "1",
@@ -26,9 +26,17 @@ export default function Works() {
       img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < 2 ? currentSlide + 1 : 0);
+  };
   return (
     <div className="works" id="works">
-      <div className="slider" style={{ transform: `` }}>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((item) => (
           <div className="container">
             <div className="item">
@@ -49,8 +57,18 @@ export default function Works() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }

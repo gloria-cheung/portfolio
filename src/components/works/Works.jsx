392387bea3e3b33fa642ff5024785906994749a1
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-08-28 10:06:21 -0400

    add dummy data to display and display arrows on either side of container to navigate slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,30 +1,56 @@
+import { useState } from "react";
 import "./Works.scss";
 
 export default function Works() {
+  const [currentSlider, setCurrentSlider] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
   return (
     <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="" />
+      <div className="slider" style={{ transform: `` }}>
+        {data.map((item) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={item.icon} alt="" />
+                  </div>
+                  <h2>{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed
-                  nisi totam molestias corporis iusto quaerat sit deserunt
-                  officiis, neque quam labore delectus illo cupiditate
-                  voluptatum, porro iste ex dolor magnam?
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img src={item.img} alt="" />
               </div>
             </div>
-            <div className="right"></div>
           </div>
-        </div>
+        ))}
       </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" />
+      <img src="assets/arrow.png" className="arrow right" alt="" />
     </div>
   );
 }

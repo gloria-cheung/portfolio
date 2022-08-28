d97e825f9a18e34549447365d4e33ddd4b54b4f2
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-08-28 09:46:02 -0400

    add markup to make slider and separating left and right container

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,7 +1,30 @@
-import "./Works.scss"
+import "./Works.scss";
 
 export default function Works() {
   return (
-    <div className="works" id="works">Works</div>
-  )
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed
+                  nisi totam molestias corporis iusto quaerat sit deserunt
+                  officiis, neque quam labore delectus illo cupiditate
+                  voluptatum, porro iste ex dolor magnam?
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

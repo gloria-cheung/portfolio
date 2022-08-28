bad960a99a14f67b0fe82d437a73791fb52dae84
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-08-28 08:58:22 -0400

    display active class when list item is clicked on

--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,6 +1,13 @@
 import "./PortfolioList.scss";
 
 export default function PortfolioList(props) {
-  const { title } = props;
-  return <li className="portfolioList">{title}</li>;
+  const { title, active, setSelected, id } = props;
+  return (
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
 }

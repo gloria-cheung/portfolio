483bcbb547fe32d3b9e707ae2f50829847d5191a
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-17 13:58:54 -0400

    add more images to portfolio

--- src/data.js
+++ src/data.js
@@ -7,27 +7,27 @@ export const featuredPortfolio = [
   },
   {
     id: 2,
+    title: "GloriasPlantsAndPots",
+    img: "assets/portfolio/gloriasplantsandpotsapp.png",
+    source: "https://gloriasplantsandpots.herokuapp.com/",
+  },
+  {
+    id: 3,
     title: "Food Order App",
     img: "assets/portfolio/foodorder.png",
     source: "https://reactmeals-app.herokuapp.com/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Tweeter",
     img: "assets/portfolio/tweeter.png",
     source: "https://tweeterclone-app.herokuapp.com/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Finders Sneakers",
     img: "assets/portfolio/finders_sneakers.png",
     source: "https://github.com/gloria-cheung/Finders-Sneakers",
-  },
-  {
-    id: 5,
-    title: "LightBnB",
-    img: "assets/portfolio/lightbnb.png",
-    source: "https://github.com/gloria-cheung/LightBnB",
   },
   {
     id: 6,
@@ -52,18 +52,24 @@ export const reactPortfolio = [
   },
   {
     id: 3,
+    title: "BakedByGloria",
+    img: "assets/portfolio/bakedbygloriaapp.png",
+    source: "https://github.com/gloria-cheung/bakedbygloria",
+  },
+  {
+    id: 4,
     title: "To Do List",
     img: "assets/portfolio/todolist.png",
     source: "https://github.com/gloria-cheung/to-do-list",
   },
   {
-    id: 4,
+    id: 5,
     title: "Hangman",
     img: "assets/portfolio/hangman.png",
     source: "https://github.com/gloria-cheung/hangman",
   },
   {
-    id: 5,
+    id: 6,
     title: "Lights Out",
     img: "assets/portfolio/lightsout.png",
     source: "https://github.com/gloria-cheung/lightsout",
@@ -73,12 +79,18 @@ export const reactPortfolio = [
 export const rubyPortfolio = [
   {
     id: 1,
+    title: "GloriasPlantsAndPots",
+    img: "assets/portfolio/gloriasplantsandpotsapp.png",
+    source: "https://gloriasplantsandpots.herokuapp.com/",
+  },
+  {
+    id: 2,
     title: "Jungle",
     img: "assets/portfolio/jungle.png",
     source: "https://github.com/gloria-cheung/jungle-rails",
   },
   {
-    id: 2,
+    id: 3,
     title: "2 Player Math Game",
     img: "assets/portfolio/mathgame.png",
     source: "https://github.com/gloria-cheung/2player_math_game",
